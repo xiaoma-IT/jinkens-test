2ce9538c74a8263fbcf4907442e86b90b55448ae
xiaoma-IT <85216056+xiaoma-IT@users.noreply.github.com>
2026-07-14 21:30:13 +0800

    Update Test.java

--- demo/src/main/java/Test.java
+++ demo/src/main/java/Test.java
@@ -1,5 +1,6 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
