9e896bebc90e79e9e1732a54786455d96eac71de
xiaoma-IT <85216056+xiaoma-IT@users.noreply.github.com>
2026-07-14 21:47:46 +0800

    Create Application.java

---
+++ demo/src/main/java/app/Application.java
@@ -0,0 +1,20 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("项目启动完成，Jenkins部署成功");
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "部署成功，网页正常访问";
+    }
+}
