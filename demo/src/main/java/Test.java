5559585dc8bd125a4a486e6e2a7bace03e5d8dad
xiaoma-IT <85216056+xiaoma-IT@users.noreply.github.com>
2026-07-14 21:41:20 +0800

    Update Test.java

--- demo/src/main/java/Test.java
+++ demo/src/main/java/Test.java
@@ -1,18 +1,20 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+// 排除R2dbc自动配置，解决默认包扫描报错
+@SpringBootApplication(exclude = R2dbcAutoConfiguration.class)
 @RestController
 public class Test {
     public static void main(String[] args) {
         SpringApplication.run(Test.class, args);
-        System.out.println("Jenkins Maven Test Success");
+        System.out.println("应用启动完成，Jenkins部署成功");
     }
 
     @GetMapping("/")
-    public String showMsg() {
-        return "部署成功，版本v23";
+    public String hello() {
+        return "部署成功，网页正常访问";
     }
 }
