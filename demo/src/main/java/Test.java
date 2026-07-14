8cf1a197258063a571373a88edf4d92f34c75052
xiaoma-IT <85216056+xiaoma-IT@users.noreply.github.com>
2026-07-14 21:10:11 +0800

    Update Test.java

--- demo/src/main/java/Test.java
+++ demo/src/main/java/Test.java
@@ -1,5 +1,18 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class Test {
     public static void main(String[] args) {
+        SpringApplication.run(Test.class, args);
         System.out.println("Jenkins Maven Test Success");
+    }
+
+    @GetMapping("/")
+    public String showMsg() {
+        return "部署成功，版本v18";
     }
 }
